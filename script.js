61379b45614556033e8a0a6851f7243b85553710
Michael <michael.ms.dev@gmail.com>
2025-03-13 01:02:59 -0300

    acrescentando botao submenu em versão para mobile

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
+    if ($(window).width() <= 640) {
+        $('.botao-menu').click(function() {
+            $('.menu').slideToggle(500);
+        });
+
+        $('.submenu-btn').click(function(event) {
+            event.stopPropagation();
+            $(this).next('.submenu').slideToggle(500);
+        });
+    }
 });
